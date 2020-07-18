78a2ebff91912acf427d702920e4b49a4f336ab4
Marcus Gabriel Martinez <marcusgabrielmartinez@users.noreply.github.com>
2020-07-18 05:08:12 -0700

    Added commands to phraselate.sh notes.

--- phraselate.sh
+++ phraselate.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 ## Run these commands before running this script.
+# wget -O ./utils/deepspeech-0.7.3-models.pbmm https://github.com/mozilla/DeepSpeech/releases/download/v0.7.3/deepspeech-0.7.3-models.pbmm
+# wget -O ./utils/deepspeech-0.7.3-models.scorer https://github.com/mozilla/DeepSpeech/releases/download/v0.7.3/deepspeech-0.7.3-models.scorer
 # virtualenv -p python3 ./env/phraselate-venv/
 # source ./env/phraselate-venv/bin/activate
 # pip3 install -r ./utils/requirements.txt
