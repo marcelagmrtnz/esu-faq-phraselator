12194609d713e784f3331756c7bd5a67308faf10
Marcus Gabriel Martinez <marcusgabrielmartinez@users.noreply.github.com>
2020-08-03 01:36:06 -0700

    Added esu model wget lines to appropriate places and created first try at install script.

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ This system has only been tested on an OSX machine.
 ```
 wget -O ./utils/deepspeech-0.7.3-models.pbmm https://github.com/mozilla/DeepSpeech/releases/download/v0.7.3/deepspeech-0.7.3-models.pbmm
 wget -O ./utils/deepspeech-0.7.3-models.scorer https://github.com/mozilla/DeepSpeech/releases/download/v0.7.3/deepspeech-0.7.3-models.scorer
+wget -O ./utils/esu_model_500epochs.pbmm https://www.dropbox.com/s/4ztgn4vvwoglo71/esu_model_500epochs.pbmm?raw=0
 virtualenv -p python3 ./env/phraselate-venv/
 source ./env/phraselate-venv/bin/activate
 pip3 install -r ./utils/requirements.txt
---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+### Run this file to install dependencies ###
+
+## Download English/esu acoustic/language models
+wget -O ./utils/deepspeech-0.7.3-models.pbmm https://github.com/mozilla/DeepSpeech/releases/download/v0.7.3/deepspeech-0.7.3-models.pbmm
+wget -O ./utils/deepspeech-0.7.3-models.scorer https://github.com/mozilla/DeepSpeech/releases/download/v0.7.3/deepspeech-0.7.3-models.scorer
+wget -O ./utils/esu_model_500epochs.pbmm https://www.dropbox.com/s/4ztgn4vvwoglo71/esu_model_500epochs.pbmm?raw=0
+wget -O ./utils/esu_lm.scorer https://www.dropbox.com/s/09tbeqs4xzlkmaj/esu_lm.scorer?raw=0
+
+## Create and source virtual environment
+python3 -m venv ./env/phraselate-venv/
+source ./env/phraselate-venv/bin/activate
+
+## Install dependencies
+pip3 install -r ./utils/requirements.txt
+python3 -c "import nltk; nltk.download('punkt');"
