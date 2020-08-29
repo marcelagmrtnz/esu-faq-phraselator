39c5e1d815820dc242a2b839047952203ece846a
Marcus Gabriel Martinez <marcusgabrielmartinez@users.noreply.github.com>
2020-08-29 01:41:33 -0700

    Fixing errors in models.sh.

--- models.sh
+++ models.sh
@@ -3,14 +3,14 @@
 ### Run this file to download models ###
 
 ## Download English acoustic/language models
-wget -O ./utils/deepspeech-0.7.3-models.pbmm https://github.com/mozilla/DeepSpeech/releases/download/v0.7.3/deepspeech-0.7.3-models.pbmm
-wget -O ./utils/deepspeech-0.7.3-models.scorer https://github.com/mozilla/DeepSpeech/releases/download/v0.7.3/deepspeech-0.7.3-models.scorer
+#wget -O ./utils/deepspeech-0.7.3-models.pbmm https://github.com/mozilla/DeepSpeech/releases/download/v0.7.3/deepspeech-0.7.3-models.pbmm
+#wget -O ./utils/deepspeech-0.7.3-models.scorer https://github.com/mozilla/DeepSpeech/releases/download/v0.7.3/deepspeech-0.7.3-models.scorer
 
 # Reconstruct Yup'ik acoustic/language models
-cat utils/esu_models/* > esu_models.tar.gz
+cat utils/esu_models/* > utils/esu_models.tar.gz
 tar -xzf utils/esu_models.tar.gz
-mv utils/esu_models/* ../
-rm -rf esu_models
+mv esu_models/* utils/
+rm -rf esu_models/; rm -rf utils/esu_models/;
 
 ## Instruct to create and source virtual environment
 echo "DONE! Run the following commands next:"
