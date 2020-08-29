cacb8d3ba911ff93d85f2956f9686478e122350a
Marcus Gabriel Martinez <marcusgabrielmartinez@users.noreply.github.com>
2020-08-29 01:44:24 -0700

    More adjustments for models.sh

--- models.sh
+++ models.sh
@@ -3,14 +3,15 @@
 ### Run this file to download models ###
 
 ## Download English acoustic/language models
-#wget -O ./utils/deepspeech-0.7.3-models.pbmm https://github.com/mozilla/DeepSpeech/releases/download/v0.7.3/deepspeech-0.7.3-models.pbmm
-#wget -O ./utils/deepspeech-0.7.3-models.scorer https://github.com/mozilla/DeepSpeech/releases/download/v0.7.3/deepspeech-0.7.3-models.scorer
+wget -O ./utils/deepspeech-0.7.3-models.pbmm https://github.com/mozilla/DeepSpeech/releases/download/v0.7.3/deepspeech-0.7.3-models.pbmm
+wget -O ./utils/deepspeech-0.7.3-models.scorer https://github.com/mozilla/DeepSpeech/releases/download/v0.7.3/deepspeech-0.7.3-models.scorer
 
 # Reconstruct Yup'ik acoustic/language models
 cat utils/esu_models/* > utils/esu_models.tar.gz
 tar -xzf utils/esu_models.tar.gz
 mv esu_models/* utils/
 rm -rf esu_models/; rm -rf utils/esu_models/;
+rm utils/esu_models.tar.gz
 
 ## Instruct to create and source virtual environment
 echo "DONE! Run the following commands next:"
