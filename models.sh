dd82c9b1c7eb28cabc8add84e68ed67d2acada25
Marcus Gabriel Martinez <marcusgabrielmartinez@users.noreply.github.com>
2020-08-29 01:28:19 -0700

    Modified downloads.sh to reconstruct split models files.

--- models.sh
+++ models.sh
@@ -2,11 +2,15 @@
 
 ### Run this file to download models ###
 
-## Download English/esu acoustic/language models
+## Download English acoustic/language models
 wget -O ./utils/deepspeech-0.7.3-models.pbmm https://github.com/mozilla/DeepSpeech/releases/download/v0.7.3/deepspeech-0.7.3-models.pbmm
 wget -O ./utils/deepspeech-0.7.3-models.scorer https://github.com/mozilla/DeepSpeech/releases/download/v0.7.3/deepspeech-0.7.3-models.scorer
-wget -O ./utils/esu_model_500epochs.pbmm https://www.dropbox.com/s/4ztgn4vvwoglo71/esu_model_500epochs.pbmm?raw=0
-wget -O ./utils/esu_lm.scorer https://www.dropbox.com/s/09tbeqs4xzlkmaj/esu_lm.scorer?raw=0
+
+# Reconstruct Yup'ik acoustic/language models
+cat utils/esu_models/* > esu_models.tar.gz
+tar -xzf utils/esu_models.tar.gz
+mv utils/esu_models/* ../
+rm -rf esu_models
 
 ## Instruct to create and source virtual environment
 echo "DONE! Run the following commands next:"
